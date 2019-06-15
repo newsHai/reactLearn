fffcd3640bccf38f96b40e8c2561276bb76ead68
newsHai <gongxiaohai1008@163.com>
2019-06-15 11:26:24 +0800

    add code (#1)

---
+++ src/useList.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class UseList extends Component {
+    render(h) {
+        return (
+            <div>Hello world</div>
+        )
+    }
+}
+
+export default UseList
